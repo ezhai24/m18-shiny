c9e1a4735687ffe886e19c47d58c0c3a0f4b1266
Emily Zhai <ezhai24@gmail.com>
2017-05-11 14:33:34 -0700

    finished exercise 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,9 +1,10 @@
 # Exercise 1: Loading functions
 
 # Set your directory
-
+setwd("~/Documents/University of Washington/Junior/Spring Quarter/Info 201/Classwork/m18-shiny/exercise-1")
 
 # Source your BuildScatter.r script, exposing your BuildScatter function
-
+source("./scripts/BuildScatter.r")
 
 # Use your BuildScatter function to draw a well labeled ggplot scatterplot of the iris data
+BuildScatter(iris, iris$Petal.Width, iris$Petal.Length, iris$Species, "Petal Width vs. Length of Different Iris Species", "Width", "Length")
--- exercise-1/scripts/BuildScatter.R
+++ exercise-1/scripts/BuildScatter.R
@@ -1,4 +1,5 @@
 # Function to build a scatterplot
+library(ggplot2)
 
 # Write your function to parameterize the following variables:
 #   - Data to use
@@ -8,3 +9,8 @@
 #   - Title of the plot (set a default of "Title")
 #   - Label for the x axis (set a default of "X Title")
 #   - Label for the y axis (set a default of "Y Title")
+BuildScatter <- function(data.set, x.value, y.value, color.map, plot.title, x.label, y.label) {
+  ggplot(data = data.set) +
+    geom_point(mapping = aes(x = x.value, y = y.value, color = color.map)) +
+    labs(title = plot.title, x = x.label, y = y.label)
+}
