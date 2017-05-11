caff559bb3401ce66c319cfb4835daa55c71fde6
Emily Zhai <ezhai24@gmail.com>
2017-05-11 14:51:34 -0700

    added inputs, fixed typo

--- exercise-2/ui.R
+++ exercise-2/ui.R
@@ -3,11 +3,18 @@
 # Create a shinyUI with a fluidPage layout
 shinyUI(fluidPage(
   
-  # Add a numbericInput to choose the number of observations
-  
+  # Add a numericInput to choose the number of observations
+  numericInput("observations",
+               "Number of Observations",
+               value = 100,
+               min = 1,
+               max = 500),
   
   # Add a selectInput that allows you to select a color from a list of choices
-  
+  selectInput("color",
+              "Color",
+              choices = c("Purple" = "#7c43c6", "Yellow" = "#fff02b", "Green" = "#1abc45", "Pink" = "#ff93de"),
+              selected = "Purple")
   
   # Plot the output with the name "scatter"
   
